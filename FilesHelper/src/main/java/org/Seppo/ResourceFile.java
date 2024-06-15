e2caffbc5c73590f57ee49e835d234425fcb61b4
realSeppo <SpbMsc25@mail.ru>
2024-06-15 21:07:52 +0300

    1.0

---
+++ FilesHelper/src/main/java/org/Seppo/ResourceFile.java
@@ -0,0 +1,34 @@
+package org.Seppo;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class ResourceFile {
+    private final String path;
+
+    public ResourceFile(String path) {
+        this.path = path;
+    }
+    public String getPath(){
+        return path;
+    }
+    public String getName(){
+        return path.substring(path.lastIndexOf("/"));
+    }
+    public String getContent(Charset charset) throws IOException {
+        return new String(getBytes(), charset);
+    }
+    public InputStream getStream() throws FileNotFoundException {
+        InputStream inputStream = ResourceFile.class.getResourceAsStream("/" + path);
+        if (inputStream == null) {
+            throw new FileNotFoundException("There is no resource on the " + path + " path");
+        }
+        return inputStream;
+    }
+    public byte[] getBytes() throws IOException {
+        return getStream().readAllBytes();
+    }
+}
---
+++ FilesHelper/src/main/java/org/Seppo/SimpleFile.java
@@ -0,0 +1,30 @@
+package org.Seppo;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SimpleFile extends File {
+    public SimpleFile(String path) {
+        super(path);
+    }
+    public SimpleFile(String path, byte[] content) {
+        super(path);
+        try {
+            Files.write(Path.of(path), content);
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+    public SimpleFile(String path, String content) {
+        super(path);
+        try {
+            new FileWriter(this).write(content);
+        } catch (IOException e) {throw new RuntimeException(e);}
+    }
+    public byte[] readBytes() throws IOException {
+        return Files.readAllBytes(Path.of(getPath()));
+    }
+    public String readContent(Charset charset) throws IOException {
+        return new String(readBytes(), charset);
+    }
+}
