be791df40bdf737e1b567243b644483d6c419752
realSeppo <SpbMsc25@mail.ru>
2024-06-16 20:25:48 +0300

    Fix

--- src/main/java/org/Seppo/SimpleFile.java
+++ src/main/java/org/Seppo/SimpleFile.java
@@ -12,6 +12,7 @@ public class SimpleFile extends File {
     public SimpleFile(String path, byte[] content, boolean rewrite) {
         super(path);
         if(!rewrite && exists()) return;
+        System.out.println(!rewrite + " " + exists());
         try {
             createNewFile();
             Files.write(Path.of(path), content);
